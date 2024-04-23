fd789c24aea7f814538533f72697b2cc5741124e
Elise <elisb8700@gmail.com>
2024-04-23 16:42:10 +0200

    route firebase

--- backend/App.py
+++ backend/App.py
@@ -1,7 +1,37 @@
 from flask import Flask
+import pyrebase
+from app.firebase_config import config
+from app.routes import *
 
+
+# Initialisation de Flask
 app = Flask(__name__)
 
+# Initialisation de Firebase
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+# Définition des routes
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/add_data', methods=['POST'])
+def add_data_route():
+    return add_data(db)
+
+@app.route('/get_data', methods=['GET'])
+def get_data_route():
+    return get_data(db)
+
+@app.route('/update_data', methods=['POST'])
+def update_data_route():
+    return update_data(db)
+
+@app.route('/delete_data', methods=['DELETE'])
+def delete_data_route():
+    return delete_data(db)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ backend/app/firebase_config.py
@@ -0,0 +1,6 @@
+config = {
+  "apiKey": "AIzaSyCkW0dKGKVOr9Cp3anGEgyCYoAjfMPgr4A",
+  "authDomain": "cryptochat-fae4d.firebaseapp.com",
+  "databaseURL": "https://cryptochat-fae4d-default-rtdb.europe-west1.firebasedatabase.app/",
+  "storageBucket": "cryptochat-fae4d.appspot.com"
+}
