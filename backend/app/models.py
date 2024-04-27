8d14f93da7806246cb7c18869cb03ec7d430edb6
Pablo Arrieumerlou <pablo.arrieumerlou@gmail.com>
2024-04-28 01:43:54 +0200

    [backend] Ajout des méthodes de transfert

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,21 +1,67 @@
-import uuid
-
 class User:
-  def __init__(self, username : str, password : str):
-    self.Name = username
-    self.Password = password
+  def __init__(self, Username : str, Password : str):
+    self.Username = Username
+    self.Password = Password
+
+  def toDict(self):
+    return {
+            'Username': self.Username,
+            'Password': self.Password
+    }
 
 class Message:
-  def __init__(self, id : uuid, sender : User, content : str):
-    self.Id = id
-    self.Sender = sender
-    self.Content = content
+  def __init__(self, Id : str, Sender : User, Content : str):
+    self.Id = Id
+    self.Sender = Sender
+    self.Content = Content
+
+  def toDict(self):
+    return {
+            'Id': self.Id,
+            'Sender': self.Sender.toDict(), 
+            'Content': self.Content
+    }
 
 class Chat:
-  def __init__(self, id : uuid, name : str, password : str, creator : User, messages : list, members : list):
-    self.Id = id
-    self.Name = name
-    self.Password = password
-    self.Creator = creator
-    self.Messages = messages
-    self.Members = members
+  def __init__(self, Id : str, Name : str, Password : str, Creator : User, Messages : list, Members : list):
+    self.Id = Id
+    self.Name = Name
+    self.Password = Password
+    self.Creator = Creator
+    self.Messages = Messages
+    self.Members = Members
+
+  def toDict(self):
+    msg_dict = []
+    for msg in self.Messages:
+      msg_dict.append(msg.toDict())
+    return {
+            'Id': self.Id,
+            'Name': self.Name, 
+            'Password': self.Password, 
+            'Creator': self.Creator.toDict(), 
+            'Message': msg_dict
+    }
+
+  def addMessage(self,Msg : Message ):
+    self.Messages.append(Msg)
+
+  def getMessages(self):
+    return [
+      {
+        "Id": message.Id,
+        "Sender": {
+            "Username": message.Sender.Username,
+            "Password": message.Sender.Password
+        },
+        "Content": message.Content
+      } for message in self.Messages
+    ]
+  
+  def getMembers(self):
+    return [
+      {
+        "Username": member.Username,
+        "Password": member.Password
+      } for member in self.Members
+    ]
