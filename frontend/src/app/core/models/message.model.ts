4fa9a64aaff37f0e8cf11a1c954f77a53833ae92
Pablo Arrieumerlou <pablo.arrieumerlou@gmail.com>
2024-04-28 01:04:54 +0200

    [frontend] maj des modèles

--- frontend/src/app/core/models/message.model.ts
+++ frontend/src/app/core/models/message.model.ts
@@ -1,14 +1,15 @@
 import { User } from "./user.model";
 
 export class Message {
-    private Id : string;
+    public Id : string;
     public Sender : User;
     public Content : string;
 
-    public constructor (id : string, sender : User, content : string) {
-        this.Id = id;
-        this.Sender = sender;
-        this.Content = content;
+
+    public constructor (id? : string, sender? : User, content? : string) {
+        this.Id = id  != undefined ? id :"";
+        this.Sender = sender != undefined ? sender : new User();
+        this.Content = content != undefined ? content : "";
     }
 
 }
--- frontend/src/app/core/models/user.model.ts
+++ frontend/src/app/core/models/user.model.ts
@@ -2,8 +2,8 @@ export class User {
     public Username : string;
     public Password : string;
 
-    public constructor (username : string, password : string) {
-        this.Username = username;
-        this.Password = password;
+    public constructor (username? : string, password? : string) {
+        this.Username = username != undefined ? username :"";
+        this.Password = password != undefined ? password : "";
     }
 }
